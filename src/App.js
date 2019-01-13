9e84d1c79cf4173f2eb8bcddc0aeae40144f8a5f
Nguyen Khanh Duc <capt4inmon@macbooks-MBP.net.fpt>
2019-01-13 17:20:25 +0700

    Carousel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Menu from './components/Menu/Menu'
+import Carousel from './components/Carousel/Carousel'
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <Menu/>
+        <Carousel/>
       </div>
     );
   }
