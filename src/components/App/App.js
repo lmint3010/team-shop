b38394d0a8d81348c5aafaf6edb73538b7ead6c2
MinPham <minh.cndc@gmail.com>
2019-01-24 00:40:21 +0700

    scroll topmenu

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container } from 'reactstrap';
 
 // Team Components
 import Menu from '../Menu/Menu'
@@ -10,12 +11,28 @@ import TopMenu from '../TopMenu/TopMenu';
 
 // Team CSS
 import './App.css';
-//12312312321
 const App = () => {
     return (
         <div className="App">
-            {/* <Menu/> */}
-            <TopMenu />
+            <div className="menu-fixed">
+                <Container>
+                    <div className="flex">
+                        <div className="top-logo">
+                            <a href="\#" title="logo">
+                                <img src={`${process.env.PUBLIC_URL}/images/logo.png`} alt="logo-phones-shope" />
+                            </a> 
+                        </div>
+                        <div className="top-nav"> 
+                            <TopMenu /> 
+                        </div>    
+                    </div>
+                </Container>
+            </div>
+            <div className="main-menu text-left">
+                <Container>
+                    <TopMenu />
+                </Container>
+            </div>
             <Carousel/>
             <ListProducts />
             <TopProducts />
