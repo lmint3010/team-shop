73bd07c3e662bbecfa9225bfc5783c6367c1f159
MinPham <minh.cndc@gmail.com>
2019-01-27 17:20:59 +0700

    search product

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Row, Col, Container } from 'reactstrap';
+import SearchProducts from './SearchProducts/SearchProducts';
 
 const Categories = () => (
-  <Container>
-    <Row>
-      <Col lg="3">Left</Col>
-      <Col lg="9">Right</Col>
-    </Row>
-  </Container>
+    <Container>
+        <Row>
+            <Col lg="3">
+                <SearchProducts />
+            </Col>
+            <Col lg="9">Right</Col>
+        </Row>
+    </Container>
 )
 
 export default Categories
