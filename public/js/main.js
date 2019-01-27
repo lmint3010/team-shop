14ecbb1311c16c3fed3c9ae7d33ba5a4cff21b66
MinPham <minh.cndc@gmail.com>
2019-01-28 02:45:40 +0700

    complete show results for

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var topmenu = document.querySelector('.menu-fixed');
-    var menu = document.querySelector('.main-menu');
+    let topmenu = document.querySelector('.menu-fixed');
+    let menu = document.querySelector('.main-menu');
     window.addEventListener('scroll', function() {
         if(window.pageYOffset > menu.offsetTop + 50) {
             topmenu.classList.add('visible');
         } else {
             topmenu.classList.remove('visible');
         }
-    })
+    });
+
+
+    // accordion
+    let accordion = document.getElementsByClassName('accordion');
+    let allPanel = document.getElementsByClassName('panel');
+    for(let i = 0; i < accordion.length; i++) {
+        accordion[i].addEventListener('click', function(event) {
+            let panel = event.target.parentElement.nextElementSibling;
+            if (panel.style.display === "block"){
+                panel.style.display = "none";
+            } else {
+                for(let j = 0; j < allPanel.length; j++) {
+                    allPanel[j].style.display = "none";
+                }
+                panel.style.display = "block";
+            }
+        })
+    }
 })
--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Row, Col, Container } from 'reactstrap';
 
 import SortBy from './SortBy/SortBy';
+import ShowResults from './ShowResults/ShowResults';
 import './Categories.scss';
 
 const Categories = () => (
   <Container>
     <Row>
-      <Col lg="3">Left</Col>
+      <Col lg="3">
+        <ShowResults />
+      </Col>
       <Col lg="9">
         <SortBy />
       </Col>
