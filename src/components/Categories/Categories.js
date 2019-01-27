6dd0b63e269dfdbb94f8b9ae04fd80634105908e
Lmint <jeonkyosin@gmail.com>
2019-01-27 13:11:46 +0700

    Categories Layout

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,5 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Row, Col, Container } from 'reactstrap';
 
-const Categories = () => <div>Categories</div>
+const Categories = () => (
+  <Container>
+    <Row>
+      <Col lg="3">Left</Col>
+      <Col lg="9">Right</Col>
+    </Row>
+  </Container>
+)
 
 export default Categories
