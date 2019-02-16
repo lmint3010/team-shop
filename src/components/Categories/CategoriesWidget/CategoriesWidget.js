4e474150cac37ea7ae79a84e5b75c9b4115a5e2e
ducnguyn <stephenmon8991@gmail.com>
2019-02-16 17:29:06 +0700

    sort to top

--- src/components/Categories/CategoriesWidget/CategoriesWidget.js
+++ src/components/Categories/CategoriesWidget/CategoriesWidget.js
@@ -1,50 +1,68 @@
 import React, { Component } from "react";
-import './CategoriesWidget.scss';
+import "./CategoriesWidget.scss";
 
 class CategoriesWidget extends Component {
+  state = {
+    categoriesItem: [
+      {
+        id: 1,
+        cateName: "Bathroom",
+        quantity: 1,
+        isChecked: false
+      },
+      {
+        id: 2,
+        cateName: "Kitchen",
+        quantity: 2,
+        isChecked: false
+      },
+      {
+        id: 3,
+        cateName: "Ballroom",
+        quantity: 3,
+        isChecked: false
+      },
+      {
+        id: 4,
+        cateName: "Livingroom",
+        quantity: 4,
+        isChecked: false
+      }
+    ]
+  };
+  onChange = id => {
+    return this.setState({
+      categoriesItem: this.state.categoriesItem.map(item => {
+        if (item.id === id) {
+          item.isChecked = !item.isChecked;
+        }
+        return item;
+      })
+    });
+  };
   render() {
     return (
       <nav className="menu-categories">
         <h2 className="title-category">Categories</h2>
-          <div className="cate-panel">
-            <div className="cate-item">
-              <input type="checkbox"/>
-              &nbsp;
-              <p>Bathroom</p>
-              <span>100</span>
-            </div>
-            <div className="cate-item">
-              <input type="checkbox"/>
-              &nbsp;
-              <p>Bathroom</p>
-              <span>100</span>
-            </div>
-            <div className="cate-item">
-              <input type="checkbox"/>
-              &nbsp;
-              <p>Bathroom</p>
-              <span>100</span>
-            </div>
-            <div className="cate-item">
-              <input type="checkbox"/>
-              &nbsp;
-              <p>Bathroom</p>
-              <span>100</span>
-            </div>
-            <div className="cate-item">
-              <input type="checkbox"/>
-              &nbsp;
-              <p>Bathroom</p>
-              <span>100</span>
-            </div>
-            <div className="cate-item">
-              <input type="checkbox"/>
-              &nbsp;
-              <p>Bathroom</p>
-              <span>100</span>
-            </div>
-            
-             </div>
+        <ul className="cate-panel">
+          {this.state.categoriesItem
+            .sort(
+              (a, b) => a.isChecked !== b.isChecked && a.isChecked === false
+            )
+            .map(item => (
+              <li className="cate-item">
+                <input
+                  type="checkbox"
+                  id={"checkbox_" + item.id}
+                  checked={item.isChecked}
+                  onChange={() => this.onChange(item.id)}
+                />
+                &nbsp;
+                <p>{item.cateName}</p>
+                <span>{item.quantity}</span>
+              </li>
+            ))}
+        </ul>
       </nav>
     );
   }
